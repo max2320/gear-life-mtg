30f3a87ad4889a01168704e3bd60e292946a9ce1
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2018-01-27 13:57:21 -0200

    add analytics script

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', process.env.GOOGLE_ANALYTICS);
