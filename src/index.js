c1f348a9e087ef2f43dd2a048b1e5151cb252be8
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2018-03-30 14:24:01 -0300

    attempt fix

--- src/index.js
+++ src/index.js
@@ -4,21 +4,5 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-try{
-  ReactDOM.render(<App />, document.getElementById('root'));
-  registerServiceWorker();
-}catch(error){
-   document.querySelector('#error').append(error)
-}
-
-//
-// function gtag(){
-//   window.dataLayer = window.dataLayer || [];
-//   dataLayer.push(arguments);
-// }
-// gtag('js', new Date());
-// gtag('config', 'UA-42927176-2');
-//
-// var gaTag = document.createElement('script');
-// gaTag.setAttribute('src', "//www.googletagmanager.com/gtag/js?id=UA-42927176-2");
-// gaTag.setAttribute('async', "true");
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
