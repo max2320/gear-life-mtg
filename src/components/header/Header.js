625d009fdec5480670a3c74fcc45003b77303d83
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2019-07-16 20:12:53 -0700

    Fix header and readme

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,7 @@ import './style.css';
 import config from '../../assets/config.svg';
 import dice from '../../assets/dice.svg';
 import reset from '../../assets/reset.svg';
+import github from '../../assets/github.svg';
 
 export default class Header extends Component {
   state = {
@@ -36,6 +37,13 @@ export default class Header extends Component {
           <img src={reset} />
         </span>
         {this.renderEditButton()}
+
+        <a
+          target='_blank'
+          className='Header-icon Header-icon--right'
+          href='https://github.com/max2320/gear-life-mtg'>
+          <img src={github} />
+        </a>
       </div>
     );
   }
