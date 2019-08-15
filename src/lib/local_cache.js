6d09ea330ea58f37985c3db2a755b0049d4f543e
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2019-08-14 21:32:15 -0700

    Cache lib

---
+++ src/lib/local_cache.js
@@ -0,0 +1,13 @@
+
+export const setCache = (key, object)=>{
+  if(JSON.stringify(localStorage.getItem(key)) !== JSON.stringify(object)){
+    localStorage.setItem(key, JSON.stringify(object));
+    return true;
+  }
+
+  return false;
+}
+
+export const getCache = (key) => {
+  return localStorage.getItem(key) && JSON.parse(localStorage.getItem(key))
+}
