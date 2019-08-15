0c1dfa1c3438a85e4ffe6819a3c9613891ecb440
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2019-08-14 21:32:44 -0700

    store fix

--- src/configs/store.js
+++ src/configs/store.js
@@ -1,11 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import reducers from '../ducks';
+import reducers from '../lib/reducers';
 
 const store = createStore(
   reducers,
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
--- src/ducks/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import team from './team';
-
-const reducer = combineReducers({
-  team
-});
-
-export default reducer;
