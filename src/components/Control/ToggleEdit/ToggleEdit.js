da2798d0fec11f9965291649ce42d7ed10bf40ab
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2020-01-06 09:17:46 -0300

    Event fix

--- src/components/Control/ToggleEdit/ToggleEdit.js
+++ src/components/Control/ToggleEdit/ToggleEdit.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class ToggleEdit extends Component {
   render() {
     if(this.props.editMode){
-      return (<button onCick={this.props.disableEdit}>finish</button>)
+      return (<button onClick={this.props.disableEdit}>finish</button>)
     } else {
-      return (<button onCick={this.props.enableEdit}>Edit</button>)
+      return (<button onClick={this.props.enableEdit}>Edit</button>)
     }
   }
 }
