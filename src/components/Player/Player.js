2ecdd66c7d946f319c097c875ba05c9fd7d8db26
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2020-01-06 09:18:07 -0300

    Player improvements

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -2,30 +2,41 @@ import React, { Component } from 'react';
 
 class Player extends Component {
   id = this.props.id;
-  state = {
-    name: this.props.name
-  }
 
   handleRemove = () => {
     this.props.removePlayer(this.id)
   }
 
-  handleNameUpdate = ({ target: { value: name }}) => {
-    this.setState({ name });
+  handleNameUpdate = ({ target: { value: name } }) => {
+    this.props.setPlayerName(this.id, name);
   }
 
-  handleSetName = () =>{
-    this.props.setPlayerName(this.id, this.state.name)
+  handleTeamUpdate = ({ target: { value: teamId } }) => {
+    this.props.setPlayerTeam(this.id, teamId)
   }
 
   render() {
     return (
       <div>
-        {JSON.stringify(Object.keys(this.props))}
-        <input value={this.state.name} onInput={this.handleNameUpdate}/>
-        <button onClick={this.handleSetName}>
-          save
-        </button>
+        <input
+          defaultValue={this.props.name}
+          onInput={this.handleNameUpdate}
+        />
+        <select
+          defaultValue={this.props.teamId}
+          onChange={this.handleTeamUpdate}
+        >
+          {this.props.teamsOptions.map(({key, label}) => {
+            return (
+              <option
+                key={key}
+                value={key}
+              >
+                {label}
+              </option>
+            );
+          })}
+        </select>
 
         <button onClick={this.handleRemove}>
           -
--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -4,9 +4,16 @@ import Player from './Player';
 
 import { actions } from '../../ducks/player';
 
-const {removePlayer, setPlayerName, setPlayerColors} = actions;
+const {removePlayer, setPlayerName, setPlayerColors, setPlayerTeam} = actions;
 
-const mapStateToProps = () => ({});
-const mapDispatchToProps = { removePlayer, setPlayerName, setPlayerColors };
+const mapStateToProps = ({ team }) => {
+  const teamsOptions = team.order.map((teamId)=>({
+    key: teamId,
+    label: team.teams[teamId].name
+  }));
+
+  return { teamsOptions };
+};
+const mapDispatchToProps = { removePlayer, setPlayerName, setPlayerColors, setPlayerTeam };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Player);
