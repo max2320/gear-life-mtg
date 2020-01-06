edf22cfcf49c4db492d4a4d4e36afc50d4e48a20
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2020-01-06 09:19:06 -0300

    Match management step 1

---
+++ src/components/MatchType/consts.js
@@ -0,0 +1,17 @@
+
+const types = [{
+  name: "Standard",
+  defaultLife: 20,
+  defaultPoison: 10,
+  teamsMembers: 1
+}, {
+  name: "Two-Headed Giant",
+  defaultLife: 30,
+  defaultPoison: 15,
+  teamsMembers: 2
+}, {
+  name: "Emperor",
+  defaultLife: 20,
+  defaultPoison: 10,
+  teamsMembers: 3
+}];
