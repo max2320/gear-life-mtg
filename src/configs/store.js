736b427c4f80ffb10da1b02cd3feecf79739acfb
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2020-02-15 21:02:30 -0300

    Fix redux load

--- src/configs/store.js
+++ src/configs/store.js
@@ -2,12 +2,21 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../lib/reducers';
 
-const store = createStore(
-  reducers,
-  compose(
+if(window.__REDUX_DEVTOOLS_EXTENSION__){
+  const middlewares = compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  );
+}else{
+  const middlewares = compose(
+    applyMiddleware(thunk)
+  );
+}
+
+
+const store = createStore(
+  reducers,
+  middlewares
 );
 
 export default store;
